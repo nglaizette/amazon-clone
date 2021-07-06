3d52ac3141ea40f48259357142949f676e678dd4
NicoG <winnipeg.francois@gmail.com>
2021-07-06 23:38:44 +0200

    Commit initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  // BEM Convention Block Element Modifier
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Clever programers, let's build the Amazon Store 🚀</h1>
+      { /* Header */}
+      { /* Home */ }
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCtnNtIET5W--bS6PKCZ4sbajdbd-SHJbY",
+    authDomain: "clone-f202a.firebaseapp.com",
+    projectId: "clone-f202a",
+    storageBucket: "clone-f202a.appspot.com",
+    messagingSenderId: "130518526710",
+    appId: "1:130518526710:web:132b033a9666836ad9dad7",
+    measurementId: "G-2X08BF1RRF"
+  };
